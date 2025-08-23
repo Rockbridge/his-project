99bae294d96aec73a70f0fd9db93d47ea56540bc
Martin Hoffmann <martin.hoffmann.73@gmail.com>
2025-08-23 14:22:10 +0200

    Use AutoConfigureMockMvc in integration test

--- his-encounter-service/src/test/java/de/his/encounter/EncounterServiceIntegrationTest.java
+++ his-encounter-service/src/test/java/de/his/encounter/EncounterServiceIntegrationTest.java
@@ -8,7 +8,7 @@ import de.his.encounter.domain.model.EncounterType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
@@ -22,7 +22,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureWebMvc
+@AutoConfigureMockMvc
 @ActiveProfiles("test")
 @Transactional
 class EncounterServiceIntegrationTest {
